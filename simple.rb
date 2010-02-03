6211f69bb1232af7e329b1499f14706c78d41e7e
Noah Gibbs <noah_gibbs@yahoo.com>
2010-02-02 21:55:20 -0800

    Make probabilities easier to eyeball

--- simple.rb
+++ simple.rb
@@ -1,9 +1,9 @@
 require "markov"
 
 data = [
-  [0.1, "low", :low],
+  [0.5, "low", :low],
   [1.0, "middle", :middle],
-  [5.0, "high", :high],
+  [2.0, "high", :high],
   [1.0, [:one, " plus something"], :compound]
 ]
 g = Markov::Generator.new data
