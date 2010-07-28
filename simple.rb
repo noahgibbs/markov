2b4a4f4ad4e068868ef4c7cf86e5dea1746ebdd5
Noah Gibbs <noah_gibbs@yahoo.com>
2010-07-27 19:36:59 -0700

    Fix compound rule in simple file

--- simple.rb
+++ simple.rb
@@ -4,7 +4,7 @@ data = [
   [0.5, "low", :low],
   [1.0, "middle", :middle],
   [2.0, "high", :high],
-  [1.0, [:one, " plus something"], :compound]
+  [1.0, [:+, :start, " plus something"], :compound]
 ]
 g = Markov::Generator.new data
 
